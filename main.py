3a8f41ba3b25354907733212d1bbaca7a5dbf68b
Dylan John <dylansejohn@gmail.com>
2023-04-04 20:32:42 +0100

    It works
    Complete pong game (you play as the right paddle); simple up and down to move and very buggy and basic, cool tho

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame as pg
-import sys
+import sys, random
 
 def ball_animation():
 
@@ -12,10 +12,39 @@ def ball_animation():
     if ball.top <= 0 or ball.bottom >= HEIGHT:
         ball_speed_y *= -1
     if ball.left <= 0 or ball.right >= WIDTH:
-        ball_speed_x *= -1
+        ball_restart()
 
     if ball.colliderect(player) or ball.colliderect(opponent):
         ball_speed_x *= -1
+
+def player_animation():
+
+    player.y += player_speed
+
+    if player.top <= 0:
+        player.top = 0
+    if player.bottom >= HEIGHT:
+        player.bottom = HEIGHT
+
+def opponent_animation():
+
+    if opponent.top < ball.y:
+        opponent.top += opponent_speed
+    if opponent.bottom > ball.y:
+        opponent.bottom -= opponent_speed
+
+    if opponent.top <= 0:
+        opponent.top = 0
+    if opponent.bottom >= HEIGHT:
+        opponent.bottom = HEIGHT
+
+def ball_restart():
+
+    global ball_speed_x,ball_speed_y
+
+    ball.center = (WIDTH/2, HEIGHT/2)
+    ball_speed_y *= random.choice((+1, -1))
+    ball_speed_x *= random.choice((+1, -1))
 
 #initial setup
 pg.init()
@@ -37,8 +66,10 @@ bg_colour = (0, 0, 0)
 light_grey = (200, 200, 200)
 
 #speeeeeeeeeeeeeeeed
-ball_speed_x = 3
-ball_speed_y = 3
+ball_speed_x = 3 * random.choice((+1, -1))
+ball_speed_y = 3 * random.choice((+1, -1))
+player_speed = 0
+opponent_speed = 7
 
 #main loop; runs once per tick
 while True:
@@ -48,8 +79,21 @@ while True:
         if event.type == pg.QUIT:
             pg.quit()
             sys.exit()
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_DOWN:
+                player_speed += 5
+            if event.key == pg.K_UP:
+                player_speed -= 5
+        if event.type == pg.KEYUP:
+            if event.key == pg.K_DOWN:
+                player_speed -= 5
+            if event.key == pg.K_UP:
+                player_speed += 5
+            
 
     ball_animation()
+    player_animation()
+    opponent_animation()
 
     #visuals
     screen.fill(bg_colour)
